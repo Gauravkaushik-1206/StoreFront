212fc87425a82f8f80559b01b9e212604d480f16
gaurav <gaurav@gauravs-MacBook-Air-2.local>
2025-07-24 16:48:45 +0530

    Create a productGrid component

--- components/Home/FeaturedProducts.tsx
+++ components/Home/FeaturedProducts.tsx
@@ -1,7 +1,18 @@
+import { fetchFeaturedProducts } from '@/utils/actions'
 import React from 'react'
+import EmptyList from '../global/EmptyList';
+import SectionTitle from '../global/SectionTitle';
+import ProductsGrid from '../products/ProductsGrid';
 
-export default function FeaturedProducts() {
+export default async function FeaturedProducts() {
+   const products = await fetchFeaturedProducts();
+
+   if(products.length === 0) return <EmptyList></EmptyList>
   return (
-    <div>FeaturedProducts</div>
+    <section className='pt-24'>
+      <SectionTitle text='featured products'></SectionTitle>
+      <ProductsGrid products={products}></ProductsGrid>
+    </section>
   )
 }
+ 
--- components/products/FavoriteToggleButton.tsx
+++ components/products/FavoriteToggleButton.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { FaHeart } from 'react-icons/fa';
+import { Button } from '../ui/button';
 
-export default function FavoriteToggleButton() {
+export default function FavoriteToggleButton({productId}:{productId: string}) {
   return (
-    <div>FavoriteToggleButton</div>
+    <Button size='icon' variant='outline' className='p-2 cursor-pointer'>
+      <FaHeart></FaHeart>
+    </Button>
   )
 }
